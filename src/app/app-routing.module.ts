fd3cd33b6be31429ee60a7328049a4447e34f5e4
Kartikeya Sharma <kartikeya1k4p3@gmail.com>
2022-12-14 21:56:59 +0530

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'profile',
+    loadChildren: () => import ('./profile/profile.module').then((m)=> m.ProfileModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import ('./login/login.module').then((m)=> m.LoginModule)
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'signup', component: SignupComponent
+  },
+
+];
 
 
 @NgModule({
@@ -13,7 +23,7 @@ import { SignupComponent } from './signup/signup.component';
   ],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    RouterModule.forChild(routes),
   ]
 })
 export class LoginModule { }
